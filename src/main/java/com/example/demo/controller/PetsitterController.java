d4d2dfbb069c5bf5a93d93edc4077a8266c61952
kangjisoo <kangjs3779@gmail.com>
2023-05-24 17:33:03 +0900

    컨트롤러, 서비스, 매퍼 펫시터 생성

--- src/main/java/com/example/demo/controller/PetsitterController.java
+++ src/main/java/com/example/demo/controller/PetsitterController.java
@@ -3,7 +3,7 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.stereotype.Controller
-@RequestMapping("map")
+@RequestMapping("/bery/petsitter")
 public class PetsitterController {
 	
 	@RequestMapping("mapTest")
