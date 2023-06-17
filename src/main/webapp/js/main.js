fa86899a968b04b9ced29505dfb980763a391cd7
lllilyk <ym0402kkk@naver.com>
2023-06-17 21:11:38 +0900

    js 수정

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,17 +1,3 @@
-/*
-main();
-function main() {
-	$('.page-wrapper').slick({
-			slidesToShow: 3,
-			slidesToScroll: 1,
-			autoplay: true,
-			autoplaySpeed: 2000,
-			nextArrow: $('.next'),
-			prevArrow: $('.prev'),
-	});
-};
-*/
-
 $(document).ready(function() {
 	$('.slick-wrapper').slick({
 		slidesToShow: 3,
