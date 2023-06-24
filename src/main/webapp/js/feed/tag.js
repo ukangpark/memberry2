0fa5062a6c9bcc511447158f03314cc9d133c80d
joy <ukang0711@naver.com>
2023-06-24 23:59:21 +0900

    태그 수정

--- src/main/webapp/js/feed/tag.js
+++ src/main/webapp/js/feed/tag.js
@@ -1,30 +1,30 @@
 let tagInputs = [];
 
-$("#addTag").click(function() {
-	const tagInput = $("#tagInput").val();
-	//$("#tagInput").val('');
-	tagInputs.push(tagInput); // tagInput의 값을 배열에 추가
-	//console.log(tagInputs);
-	
-	$(".tagsContainer").append(`
-   		<span class="ui tag blue tiny label" style="margin: 2px 6px 2px 6px">${tagInput} <i class="icon mini close"></i></span>
-    `);
-    $("#addForm").append(`
-    	<input type="text" name="keyword" value="${tagInput}" style="display:none">
-    `)
-	//tagList();
+$("#tagInput").on("keyup", function(event) {
+    if (event.keyCode === 13) {
+        const tagInput = $("#tagInput");
+        const tagValue = tagInput.val().trim();
+        tagInput.val('');
 
+        if (tagValue !== '') {
+            tagInputs.push(tagValue);
+
+            $(".tagsContainer").append(`
+                <span class="ui tag blue tiny label" style="margin: 2px 6px 2px 6px">${tagValue} <i class="icon mini close"></i></span>
+            `);
+            $("#addForm").append(`
+                <input type="text" name="keyword" value="${tagValue}" style="display:none">
+            `);
+        }
+    }
 });
 
-// 엔터 시, add tag버튼 실행
-/*$("#tagInput").on("keyup", function(event) {
-	if (event.keyCode === 13) { // Enter 키의 keyCode는 13
-		$("#addTag").click(); // Add Tag 버튼 클릭 이벤트를 실행
-	}
-});*/
 
 // 태그 삭제 코드
 $(document).on("click", ".icon.mini.close", function() {
-	const tagLabel = $(this).closest(".ui.tag.blue.tiny.label");
-	tagLabel.remove();
-})
+    const tagLabel = $(this).closest(".ui.tag.blue.tiny.label");
+    const tagInputValue = tagLabel.text().trim();
+    
+    tagLabel.remove();
+    $("input[name='keyword'][value='" + tagInputValue + "']").remove();
+});
